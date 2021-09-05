c7485c1e89e54ca18a667235059a53af0480de7d
Aly <87067002+Alydiabira@users.noreply.github.com>
2021-09-05 13:43:24 +0200

    Ajout du fichier index.html

--- premierprojets/login.php
+++ premierprojets/login.php
@@ -1,97 +1,168 @@
-<form action="action_page.php" style="border:1px solid #ccc">
-  <div class="container">
-    <h1>S'inscrire</h1>
-    <p>Veuillez remplir ce formulaire pour créer un compte.</p>
-    <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+<div class="container">
+  <form action="/action_page.php">
+    <div class="row">
+      <h2 style="text-align:center">Connectez-vous avec les médias sociaux ou manuellement</h2>
+      <div class="vl">
+        <span class="vl-innertext">ou</span>
+      </div>
 
-    <label for="psw"><b>Mot de passe</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+      <div class="col">
+        <a href="#" class="fb btn">
+          <i class="fa fa-facebook fa-fw"></i> Se connecter avec Facebook
+        </a>
+        <a href="#" class="twitter btn">
+          <i class="fa fa-twitter fa-fw"></i> Login with Twitter
+        </a>
+        <a href="#" class="google btn">
+          <i class="fa fa-google fa-fw"></i> Connectez-vous avec Google+
+        </a>
+      </div>
 
-    <label for="psw-repeat"><b>Répéter le mot de passe</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+      <div class="col">
+        <div class="hide-md-lg">
+          <p>Ou connectez-vous manuellement :</p>
+        </div>
 
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+      </div>
 
-    <p>En créant un compte, vous acceptez nos <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+    </div>
+  </form>
+</div>
 
-    <div class="clearfix">
-      <button type="button" class="cancelbtn">Annuler</button>
-      <button type="submit" class="signupbtn">S'inscrire</button>
+<div class="bottom-container">
+  <div class="row">
+    <div class="col">
+      <a href="#" style="color:white" class="btn">Sign up</a>
+    </div>
+    <div class="col">
+      <a href="#" style="color:white" class="btn">mot de passe oublié?</a>
     </div>
   </div>
-</form>
+</div>
 <style>
     * {box-sizing: border-box}
 
-/* Full-width input fields */
-  input[type=text], input[type=password] {
+/* style the container */
+.container {
+  position: relative;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px 0 30px 0;
+} 
+
+/* style inputs and link buttons */
+input,
+.btn {
   width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
+  padding: 12px;
   border: none;
-  background: #f1f1f1;
+  border-radius: 4px;
+  margin: 5px 0;
+  opacity: 0.85;
+  display: inline-block;
+  font-size: 17px;
+  line-height: 20px;
+  text-decoration: none; /* remove underline from anchors */
 }
 
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
+input:hover,
+.btn:hover {
+  opacity: 1;
 }
 
-hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
+/* add appropriate colors to fb, twitter and google buttons */
+.fb {
+  background-color: #3B5998;
+  color: white;
 }
 
-/* Set a style for all buttons */
-button {
+.twitter {
+  background-color: #55ACEE;
+  color: white;
+}
+
+.google {
+  background-color: #dd4b39;
+  color: white;
+}
+
+/* style the submit button */
+input[type=submit] {
   background-color: #04AA6D;
   color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
   cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
 }
 
-button:hover {
-  opacity:1;
+input[type=submit]:hover {
+  background-color: #45a049;
 }
 
-/* Extra styles for the cancel button */
-.cancelbtn {
-  padding: 14px 20px;
-  background-color: #f44336;
-}
-
-/* Float cancel and signup buttons and add an equal width */
-.cancelbtn, .signupbtn {
+/* Two-column layout */
+.col {
   float: left;
   width: 50%;
+  margin: auto;
+  padding: 0 50px;
+  margin-top: 6px;
 }
 
-/* Add padding to container elements */
-.container {
-  padding: 16px;
-}
-
-/* Clear floats */
-.clearfix::after {
+/* Clear floats after the columns */
+.row:after {
   content: "";
-  clear: both;
   display: table;
+  clear: both;
 }
 
-/* Change styles for cancel button and signup button on extra small screens */
-@media screen and (max-width: 300px) {
-  .cancelbtn, .signupbtn {
+/* vertical line */
+.vl {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  border: 2px solid #ddd;
+  height: 175px;
+}
+
+/* text inside the vertical line */
+.inner {
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #f1f1f1;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  padding: 8px 10px;
+}
+
+/* hide some text on medium and large screens */
+.hide-md-lg {
+  display: none;
+}
+
+/* bottom container */
+.bottom-container {
+  text-align: center;
+  background-color: #666;
+  border-radius: 0px 0px 4px 4px;
+}
+
+/* Responsive layout - when the screen is less than 650px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 650px) {
+  .col {
     width: 100%;
+    margin-top: 0;
+  }
+  /* hide the vertical line */
+  .vl {
+    display: none;
+  }
+  /* show the hidden text on small screens */
+  .hide-md-lg {
+    display: block;
+    text-align: center;
   }
 }
 </style>
